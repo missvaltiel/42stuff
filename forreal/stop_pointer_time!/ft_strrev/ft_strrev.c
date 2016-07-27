5442891f68eb0788961654c1043c820bcbcb05b5
missvaltiel <missvaltiel@yahoo.com>
2016-07-26 21:32:58 -0700

    yo

--- forreal/stop_pointer_time!/ft_strrev/ft_strrev.c
+++ forreal/stop_pointer_time!/ft_strrev/ft_strrev.c
@@ -2,6 +2,8 @@
 
 char	*ft_strrev(char *str);
 void	ft_putchar(char c);
+int	ft_strlen(char *str);
+void	ft_putstr(char *str);
 
 int	main()
 {
@@ -14,14 +16,42 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-char	*ft_strrev(char *str)
-{
+int	ft_strlen(char *str)
+{	
 	int	i;
 	
-	if (i = '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		ft_putchar(str[i]);
-		i--;
+		i++;
 	}
-	return(str);
+	return(i);
+}
+
+void	ft_putstr(char *str)
+{
+	int ind;
+
+	ind = 0;
+	while (ind != '\0')
+	{
+		ft_putchar(str[ind]);
+		ind++;
+	}
+}
+
+char	*ft_strrev(char *str)
+{
+	char	*str2;
+	int	len;
+	int	j;
+
+	j = 0;
+	len = ft_strlen(str);
+	while (str2[len] = str[j])
+	{
+		j++;
+		len--;
+	}
+	return(str2);
 }
